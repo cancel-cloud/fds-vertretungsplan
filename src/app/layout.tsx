0da15763909ed710fa19234085ff1edb25e3288f
cancel-cloud <Laggis_131@icloud.com>
2024-06-06 19:35:37 +0200

    now analytics?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
-import React from "react";
-import {PHProvider} from "@/app/providers";
-import { Analytics } from '@vercel/analytics/react';
-import dynamic from "next/dynamic";
 
-const inter = Inter({subsets: ["latin"]});
+
+import { Analytics } from '@vercel/analytics/react';
+import {Metadata} from "next";
+
 
 export const metadata: Metadata = {
     title: "FDS Vertretungsplan",
     description: "Dieser Vertretungsplan, ist sortierbar, schöner anzusehen und einfacher zu verstehen.",
 };
 
-const PostHogPageView = dynamic(() => import('@/app/PostHogPageView'), {
-    ssr: false,
-});
 
 export default function RootLayout({
                                        children,
@@ -22,11 +16,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body>
-            {children}
-            <Analytics />
-            </body>
+        <html lang="en">
+        <head>
+            <title>FDS Vertretungsplan</title>
+        </head>
+        <body>
+        {children}
+        <Analytics />
+        </body>
         </html>
     );
 }
