b5bad2f1032358d5f1a9c3335538852ae4690f68
cancel-cloud <Laggis_131@icloud.com>
2024-06-06 21:19:39 +0200

    added actual mail contact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,15 @@ export default function RootLayout({
         <html lang="en">
         <head>
             <title>FDS Vertretungsplan</title>
+            <meta property="og:image" content="<generated>"/>
+            <meta property="og:image:type" content="<generated>"/>
+            <meta property="og:image:width" content="<generated>"/>
+            <meta property="og:image:height" content="<generated>"/>
+
         </head>
         <body>
         {children}
-        <Analytics />
+        <Analytics/>
         </body>
         </html>
     );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-gray-800 text-white text-center py-4 mt-10">
-            <p>Fragen? <a href="mailto:lukas.dienst@example.com" className="underline">lukas.dienst@example.com</a></p>
+            <p>Fragen? <a href="mailto:0rare-reputed@icloud.com" className="underline">Melde dich per Mail bei mir!</a></p>
         </footer>
     );
 };
